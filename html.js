b171de24b13c52e8e382dc8ebb8fcecffb233f11
zhejun su <suzhejunk9@gmail.com>
2024-10-09 13:37:32 -0400

    create alert message

---
+++ html.js
@@ -0,0 +1,26 @@
+//popup two seperate alerts that shows the message and the secret combination upon opening the site
+//create a popup alert showing the message
+//generate variables with values, the data type should be numbers
+//apply arithmetical calculations on variables to get the secret combination
+//creat the second popup alert showing the secret combination
+
+//first popup alert
+alert(
+  `You have received this message because you have been chosen to open an important vault.`
+)
+
+//variables
+const a = 13
+const b = 3
+let secondNumber = 80
+
+//arithmetical calculations
+const firstNumber = a - b
+secondNumber = 80/2
+const thirdNumber = a * b
+
+//second popup alert
+alert(
+  `Here is the secret conbination: ${firstNumber} ${secondNumber} ${thirdNumber}`
+)
+
